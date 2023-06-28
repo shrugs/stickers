d773c122d7f1e9dab93adce4696d4e0141a70e4b
shrugs <mattgcondon@gmail.com>
2023-06-28 19:58:58 -0400

    checkpoint

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import {frxETHMinter} from "./interfaces/frxETHMinter.sol";
+
+/// @title Vault that backs stickers with frxETH
 contract Vault {
-    constructor() {}
+    frxETHMinter public immutable $minter;
+
+    constructor(frxETHMinter minter) {
+        $minter = minter;
+    }
 }
--- src/interfaces/frxETHMinter.sol
+++ src/interfaces/frxETHMinter.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
+import {IsfrxETH} from "./IsfrxETH.sol";
+
 interface frxETHMinter {
-    function sfrxETHToken() public returns (IsfrxETH sfrxETH);
+    function sfrxETHToken() external returns (IsfrxETH sfrxETH);
 
     function submitAndDeposit(
         address recipient
